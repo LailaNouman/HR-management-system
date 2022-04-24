9228aa05ac62cd2c3465fcbe0ea04c8264bd8a9b
LailaNouman <lailaibraheem@hotmail.com>
2022-04-24 23:34:01 +0300

    Task8done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let arr =[];
+let div = document.getElementById('div');
 
 function Employee(employeeId,fullName,department,level,image){
          this.employeeId = employeeId;
@@ -29,16 +30,38 @@ Employee.prototype.randomSalary = function (){
 }
 
  Employee.prototype.render = function (){
-   document.write(`<h2> ${this.fullName} ` + ` ${this.salary} </h2> <br> <br>`);
+   let smalldiv = document.createElement('div');
+   let imgg = document.createElement('img');
+   imgg.src = this.image;
+   imgg.style.width = "100px";
+   smalldiv.appendChild(imgg);
+   let header = document.createElement('h5');
+   header.textContent = " ID: " + this.employeeId + " -Name: " + this.fullName + " -Department: " + this.department
+   +" -Level: " + this.level;
+   smalldiv.appendChild(header);
+   let header1 = document.createElement('h5');
+   header1.textContent = this.salary;
+   smalldiv.appendChild(header1);
+   smalldiv.style.margin = '20px';
+   smalldiv.style.backgroundColor = 'rgb(78, 148, 79)';
+   smalldiv.style.width = '200px'
+   smalldiv.style.height = '180px'
+   smalldiv.style.textAlign = 'center';
+   smalldiv.style.padding = '.9rem 2%';
+   smalldiv.style.fontSize = '16px';
+   smalldiv.style.color = 'wheat';
+   div.appendChild(smalldiv);
+  
+
 }
 
-let Ghazi = new Employee(1000, "Ghazi Samer", "Adminstration", "Senior");
-let Lana = new Employee(1001, "Lana Ali", "Finance", "Senior");
-let Tamara = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior");
-let Safi = new Employee(1003, "Safi Walid", "Adminstration", "Mid-Senior");
-let Omar = new Employee(1004, "Omar Zaid", "Development", "Senior");
-let Rana = new Employee(1005, "Rana Saleh", "Development", "Junior");
-let Hadi = new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior");
+let Ghazi = new Employee(1000, "Ghazi Samer", "Adminstration", "Senior","Ghazi.jpg");
+let Lana = new Employee(1001, "Lana Ali", "Finance", "Senior", "Lana.jpg");
+let Tamara = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", "Tamara.jpg");
+let Safi = new Employee(1003, "Safi Walid", "Adminstration", "Mid-Senior", "Safi.jpg");
+let Omar = new Employee(1004, "Omar Zaid", "Development", "Senior", "Omar.jpg");
+let Rana = new Employee(1005, "Rana Saleh", "Development", "Junior", "Rana.jpg");
+let Hadi = new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior", "Hadi.jpg");
 
 console.log(arr);
 
@@ -46,3 +69,31 @@ for (let i = 0; i < arr.length; i++){
      arr[i].randomSalary();
     arr[i].render();
 }
+
+    function uniqueId(){
+    return Math.floor(1000 + Math.random() * 9000);
+}
+
+
+let form = document.getElementById('form');
+form.addEventListener("submit" , handleSubmit);
+
+function handleSubmit(event){
+    event.preventDefault();
+    let newId = uniqueId();
+    let full = event.target.fname.value;
+    let department = event.target.departments.value;
+    let level = event.target.level.value;
+    let img = event.target.image.value;
+    console.log(full,department,level);
+    let newEmployee = new Employee (newId, full, department, level, img);
+    newEmployee.randomSalary(); 
+    newEmployee.render();
+}
+// let imgg = document.createElement('img');
+// imgg.setAttribute("src", "Ghazi.jpg");
+// div.appendChild(imgg);
+// imgg.style.width="100px";
+// let header4 = document.createElement('h4');
+// header4.textContent = ' Name: Ghazi Samer, Department: Administrator, Level: Senior ';
+// div.appendChild(header4);
